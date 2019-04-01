599b2cecbd873908100393035fe4000766f80ea0
Justin Chen <jchen06@stuy.edu>
2019-04-01 10:17:02 -0400

    classwork 4/1, wrote helper countDigits method

--- Radix.java
+++ Radix.java
@@ -1,5 +1,24 @@
 public class Radix {
-  public void radixsort(int[] data) {
-    
+  public static void radixsort(int[] data) {
+    int repeatTimes, counter = 0;
+    for (int x = 0; x < data.length; x++) {
+      counter = countDigits(data[x]);
+      if (counter > repeatTimes) repeatTimes = counter;
+    }
+    System.out.println(repeatTimes);
+  }
+
+  private static int countDigits(int num) {
+    int digits = 0;
+    while (num > 0) {
+      num = num / 10;
+      digits = digits + 1;
+    }
+    return digits;
+  }
+
+  public static void main(String[] args) {
+    int[] testArr = {123,4,5,2,1212,0};
+    radixsort(testArr);
   }
 }
