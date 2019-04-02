01fa840cd4c95985ca7626af26ce00fe5d43e1c4
Justin Chen <jchen06@stuy.edu>
2019-04-02 18:23:32 -0400

    shorten runtime to find max amt of digits, create buckets

--- Radix.java
+++ Radix.java
@@ -1,11 +1,29 @@
+import java.util.*;
 public class Radix {
+  @SuppressWarnings("unchecked")
   public static void radixsort(int[] data) {
-    int repeatTimes, counter = 0;
+    /*
+    int repeatTimes = 0;
+    int counter = 0;
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     for (int x = 0; x < data.length; x++) {
       counter = countDigits(data[x]);
       if (counter > repeatTimes) repeatTimes = counter;
     }
-    System.out.println(repeatTimes);
+    counter = 0;
+    while (counter < repeatTimes) {
+
+    }
+    */
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    int divNum = 10;
+    int curNum = 0;
+    for (int x = 0; x < data.length; x++) {
+      curNum = data[x]%10;
+      System.out.println(curNum);
+      buckets[Math.abs(curNum)].add(data[x]);
+    }
+    System.out.println(buckets);
   }
 
   private static int countDigits(int num) {
@@ -18,7 +36,7 @@ public class Radix {
   }
 
   public static void main(String[] args) {
-    int[] testArr = {123,4,5,2,1212,0};
+    int[] testArr = {12,13,21,4,43,32,0,1};
     radixsort(testArr);
   }
 }
